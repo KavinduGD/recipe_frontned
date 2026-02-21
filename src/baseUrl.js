bf29c2d5e1129b5d02c1a89a19cbe5463306d024
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2026-02-21 13:45:57 +0530

    docker image changed

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const recipeAxios = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    "https://3ff6d494-4911-4fd6-9a0b-75b731ad9b18-prod.e1-us-east-azure.choreoapis.dev/default/recipe-backend/v1.0",
 });
 
 export default recipeAxios;
