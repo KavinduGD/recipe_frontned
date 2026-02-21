b15d8df56e1dc2e004e967f8f836dda6119d48be
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2026-02-21 13:46:26 +0530

    base url add

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//
 const recipeAxios = axios.create({
   baseURL:
     "https://3ff6d494-4911-4fd6-9a0b-75b731ad9b18-prod.e1-us-east-azure.choreoapis.dev/default/recipe-backend/v1.0",
